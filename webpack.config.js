9176fcdd3e56441e33245d2b23be9983aeb3bed4
Diego Polaco <dieghard@gmail.com>
2021-02-16 19:39:13 -0300

    uso de HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,32 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
     module: {
         rules: [
             {
-                test   : /\.js$/i ,
+                test: /\.js$/i,
                 exclude: /node_modules/,
-                use    : {
-                    loader:"babel-loader",
-                }
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.html/i,
+                use: [
+                    {
+                        loader  : "html-loader",
+                        options: {
+                            minimize: true,
+                        }
+                    }
+                ],
             }
-        ]
-    }
-}
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template : "./src/index.html",
+            filename : ".index.html",
+        }),
+    ],
+};
